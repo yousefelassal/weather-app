fb988e903540990c97f007d6cd2e435f8ea83136
Yousef <yousefsameh24@gmail.com>
2023-06-17 19:31:39 +0300

    create settings view

---
+++ src/views/Settings.js
@@ -0,0 +1,19 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor(params) {
+        super(params);
+        this.setTitle("Settings");
+    }
+
+    async getHtml() {
+        return `
+            <h1>Settings</h1>
+            <p>Manage your privacy and configuration.</p>
+        `;
+    }
+    
+    async getJs() {
+        console.log("Settings.js");
+    }
+}
