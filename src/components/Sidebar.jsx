f9a72abcbe82cde3696a020906355872758efa22
Yousef <yousefsameh24@gmail.com>
2023-07-03 23:55:47 +0300

    update sidebar size

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -41,7 +41,7 @@ const Sidebar = () => {
 
     const navsFooter = [
         {
-            to: 'javascript:void(0)',
+            to: '/',
             name: 'Help',
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
@@ -58,7 +58,7 @@ const Sidebar = () => {
             ,
         },
         {
-            to: 'javascript:void(0)',
+            to: '/',
             name: 'Logout',
             icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
@@ -70,7 +70,7 @@ const Sidebar = () => {
     return (
         <>
             <nav
-                className="fixed top-0 left-0 w-full h-full border-r border-r-gray-200 bg-white space-y-8 sm:w-64">
+                className="fixed top-0 left-0 h-full border-r border-r-gray-200 bg-white space-y-8 w-64">
                 <div className="flex flex-col h-full">
                     <div className='h-20 flex items-center px-8'>
                         <NavLink to='/' className='flex items-center -ml-7'>
